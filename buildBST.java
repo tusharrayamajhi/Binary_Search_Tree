9b8c870dcd4d60e28703a552cecfb56410ca7148
Tushar Rayamajhi <84193854+tusharrayamajhi@users.noreply.github.com>
2023-05-22 22:55:32 +0545

    update

---
+++ buildBST.java
@@ -0,0 +1,40 @@
+public class buildBST{
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data =data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    public static Node BUildBST(Node root,int n){
+       if(root == null){
+        return new Node(n);
+       }
+       if(root.data < n){
+        root.right = BUildBST(root.right, n);
+       }else{
+        root.left = BUildBST(root.left, n);
+       }
+       return root;
+
+    }
+    public static void inordertravels(Node root){
+        if(root == null){
+            return;
+        }
+        inordertravels(root.left);
+        System.out.print(root.data + " ");
+        inordertravels(root.right);
+    }
+    public static void main(String[] args) {
+        int nodes[] = {5,1,3,4,2,1};
+        Node root = null;
+        for(int i =0;i<nodes.length -1;i++){
+            root = BUildBST(root,nodes[i]);
+        }
+        inordertravels(root);
+    }
+}
